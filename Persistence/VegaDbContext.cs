10c05e5c40db68af6d7a7fba0a25c180c43bd2e0
Rafael Carneiro <rafaferreiracarneiro@gmail.com>
2018-02-18 23:44:56 -0300

    new: vehicle, contact and vehiclefeature models

--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -5,8 +5,21 @@ namespace vega.Persistence
 {
     public class VegaDbContext : DbContext 
     {
-        public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options) {}
         public DbSet<Make> Makes { get; set; }
         public DbSet<Feature> Features { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+        public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleFeature>().HasKey(vf => new { 
+                vf.VehicleId, vf.FeatureId 
+            });
+            
+            modelBuilder.Entity<Vehicle>().OwnsOne(p => p.Contact);
+            
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
