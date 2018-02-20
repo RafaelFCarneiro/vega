b4100bac78dbf87b4aaf818433ca84e8df98bd64
Rafael Carneiro <rafaferreiracarneiro@gmail.com>
2018-02-19 21:49:52 -0300

    new: API contract "api/vehicles"

---
+++ Controllers/Resources/VehicleResource.cs
@@ -0,0 +1,15 @@
+using System.Collections.ObjectModel;
+
+namespace vega.Controllers.Resources
+{
+    public class VehicleResource
+    {
+        public int Id { get; set; }
+        public int ModelId { get; set; }
+        public bool IsRegistered { get; set; }
+        public ContactResource Contact { get; set; }
+        public Collection<int> Features { get; set; }
+
+        public VehicleResource() => Features = new Collection<int>();
+    }
+}
