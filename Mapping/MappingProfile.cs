0d70e5071db3f3805ddf43ca36e78d7266703b6f
Rafael Carneiro <rafaferreiracarneiro@gmail.com>
2018-02-20 22:24:22 -0300

    refactor: use LINQ at MappingProfile to vehicle and features

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -7,39 +7,39 @@ using vega.Models;
 
 namespace vega.Mapping
 {
-    public class MappingProfile : Profile {
-        public MappingProfile() {
-            // Domain to API Resource
-            CreateMap<Make, MakeResource>();
-            CreateMap<Model, ModelResource>();
-            CreateMap<Feature, FeatureResource>();
-            CreateMap<Contact, ContactResource>();
-            CreateMap<Vehicle, VehicleResource>()
-                .ForMember(vr => vr.Features, opt => opt.MapFrom(
-                    v => v.Features.Select(vf => vf.FeatureId)
-                ));
+  public class MappingProfile : Profile
+  {
+    public MappingProfile()
+    {
+      // Domain to API Resource
+      CreateMap<Make, MakeResource>();
+      CreateMap<Model, ModelResource>();
+      CreateMap<Feature, FeatureResource>();
+      CreateMap<Contact, ContactResource>();
+      CreateMap<Vehicle, VehicleResource>()
+        .ForMember(vr => vr.Features, opt => { 
+            opt.MapFrom(v => v.Features.Select(vf => vf.FeatureId));
+        });
 
-            // API Resource to Domain            
-            CreateMap<ContactResource, Contact>();
-            CreateMap<VehicleResource, Vehicle>()
-                .ForMember(v => v.Features, opt => opt.Ignore())
-                .ForMember(v => v.Id, opt => opt.Ignore())
-                .AfterMap((vr, v) => {
-                    // Remove unselected features
-                    var removeFeatures = new List<VehicleFeature>();
-                    foreach (var f in v.Features)
-                        if(!vr.Features.Contains(f.FeatureId))
-                            removeFeatures.Add(f);
-                    foreach (var f in removeFeatures)
-                        v.Features.Remove(f);
-
-                    // Add new features
-                    foreach (var id in vr.Features)
-                        if(!v.Features.Any(f => f.FeatureId == id))
-                            v.Features.Add(new VehicleFeature { 
-                                FeatureId = id 
-                            });
-                });
-        }
+      // API Resource to Domain            
+      CreateMap<ContactResource, Contact>();
+      CreateMap<VehicleResource, Vehicle>()
+        .ForMember(v => v.Features, opt => opt.Ignore())
+        .ForMember(v => v.Id, opt => opt.Ignore())
+        .AfterMap((vr, v) => {          
+          // Remove unselected features
+          var removeFeatures = v.Features
+            .Where(f => !vr.Features.Contains(f.FeatureId));
+          foreach (var f in removeFeatures)
+            v.Features.Remove(f);
+          
+          // Add new features
+          var addedFeatures = vr.Features
+            .Where(id => !v.Features.Any(f => f.FeatureId == id))
+            .Select(id => new VehicleFeature { FeatureId = id });          
+          foreach (var f in addedFeatures)
+            v.Features.Add(f);
+        });
     }
+  }
 }
