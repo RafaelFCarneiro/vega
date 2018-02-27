c2a14b75f0886ee56220a69cc1a0ab7e3842b3fb
Rafael Carneiro <rafaferreiracarneiro@gmail.com>
2018-02-26 22:35:41 -0300

    new: use sentry.io to logs

--- ClientApp/app/app.browser.module.ts
+++ ClientApp/app/app.browser.module.ts
@@ -1,21 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppModuleShared } from './app.shared.module';
-import { AppComponent } from './components/app/app.component';
+import * as Raven from "raven-js";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppModuleShared } from "./app.shared.module";
+import { AppComponent } from "./components/app/app.component";
+
+Raven.config(
+  "https://13898b65d4d14b789c995a0466311370@sentry.io/294919"
+).install();
 
 @NgModule({
-    bootstrap: [ AppComponent ],
-    imports: [
-        BrowserModule,
-        AppModuleShared
-    ],
-    providers: [
-        { provide: 'BASE_URL', useFactory: getBaseUrl }
-    ]
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, AppModuleShared],
+  providers: [{ provide: "BASE_URL", useFactory: getBaseUrl }]
 })
-export class AppModule {
-}
+export class AppModule {}
 
 export function getBaseUrl() {
-    return document.getElementsByTagName('base')[0].href;
+  return document.getElementsByTagName("base")[0].href;
 }
