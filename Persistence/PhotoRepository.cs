6aef5b6a0e1209cce90fc3728d180f2fe8e9010c
Rafael Carneiro <rafaferreiracarneiro@gmail.com>
2018-03-14 22:58:11 -0300

    new: photo repository

---
+++ Persistence/PhotoRepository.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using vega.Core;
+using vega.Core.Models;
+
+namespace vega.Persistence
+{
+  public class PhotoRepository : IPhotoRepository
+  {
+    public VegaDbContext context { get; }
+    public PhotoRepository(VegaDbContext context) => this.context = context;
+    public async Task<IEnumerable<Photo>> GetPhotos(int vehicleId)
+    {
+      return await context.Photos
+        .Where(p => p.VehicleId == vehicleId)
+        .ToListAsync();
+    }
+  }
+}
