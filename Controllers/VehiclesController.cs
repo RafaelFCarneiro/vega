7008b651b1854194b2e22e31e72d8ade7e16829a
Rafael Carneiro <rafaferreiracarneiro@gmail.com>
2018-03-20 21:53:17 -0300

    new: add Auth0 to authorizations

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -7,6 +7,7 @@ using vega.Controllers.Resources;
 using vega.Core.Models;
 using vega.Core;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
 
 namespace vega.Controllers
 {
@@ -25,6 +26,7 @@ namespace vega.Controllers
     }
 
     [HttpPost]
+    [Authorize]
     public async Task<IActionResult> CreateVehicle([FromBody] SaveVehicleResource vehicleResource)
     {
       if (!ModelState.IsValid)
@@ -44,6 +46,7 @@ namespace vega.Controllers
     }
 
     [HttpPut("{id}")]
+    [Authorize]
     public async Task<IActionResult> UpdateVehicle(int id, [FromBody] SaveVehicleResource vehicleResource)
     {
       if (!ModelState.IsValid)
@@ -64,6 +67,7 @@ namespace vega.Controllers
     }
 
     [HttpDelete("{id}")]
+    [Authorize]
     public async Task<IActionResult> DeleteVehicle(int id)
     {
       var vehicle = await repository.GetVehicle(id, includeRelated: false);
--- Startup.cs
+++ Startup.cs
@@ -13,57 +13,78 @@ using vega.Persistence;
 using AutoMapper;
 using vega.Core;
 using vega.Core.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace vega
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration) {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) {
-            services.Configure<PhotoSettings>(Configuration.GetSection("PhotoSettings"));
-            services.AddScoped<IPhotoRepository, PhotoRepository>();
-            services.AddScoped<IVehicleRepository, VehicleRepository>();
-            services.AddScoped<IUnitOfWork, UnitOfWork>();
-            services.AddAutoMapper();
-            services.AddDbContext<VegaDbContext>(options => options.UseSqlServer(
-                Configuration.GetConnectionString("Default"))
-            );
-            services.AddMvc();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory) {
-            loggerFactory.AddConsole(Configuration.GetSection("Loging"));
-            loggerFactory.AddDebug();
-            
-            if (env.IsDevelopment()) {                
-                app.UseDeveloperExceptionPage();
-                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions {
-                    HotModuleReplacement = true
-                });
-            } else {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
-            app.UseStaticFiles();
-            
-            app.UseMvc(routes => {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}"
-                );
-
-                routes.MapSpaFallbackRoute(
-                    name: "spa-fallback",
-                    defaults: new { controller = "Home", action = "Index" }
-                );
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.Configure<PhotoSettings>(Configuration.GetSection("PhotoSettings"));
+      services.AddScoped<IPhotoRepository, PhotoRepository>();
+      services.AddScoped<IVehicleRepository, VehicleRepository>();
+      services.AddScoped<IUnitOfWork, UnitOfWork>();
+      services.AddAutoMapper();
+      services.AddDbContext<VegaDbContext>(options => options.UseSqlServer(
+          Configuration.GetConnectionString("Default"))
+      );
+
+      services.AddMvc();
+
+      // 1. Add Authentication Services
+      services.AddAuthentication(options => {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+      }).AddJwtBearer(options => {
+        options.Authority = "https://vegangproject.auth0.com/";
+        options.Audience = "https://api.vegangproject.com";
+      });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+      loggerFactory.AddConsole(Configuration.GetSection("Loging"));
+      loggerFactory.AddDebug();
+
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+        {
+          HotModuleReplacement = true
+        });
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+      }
+
+      app.UseAuthentication();
+
+      app.UseStaticFiles();
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+            name: "default",
+            template: "{controller=Home}/{action=Index}/{id?}"
+        );
+
+        routes.MapSpaFallbackRoute(
+            name: "spa-fallback",
+            defaults: new { controller = "Home", action = "Index" }
+        );
+      });
+    }
+  }
 }
